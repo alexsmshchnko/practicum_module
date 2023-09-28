db4d8d141895a9b713ba5fc2fa1f25c2ba92e389
Aleksandr Semashchenko <asemashchenko@openintegration.inc>
2023-09-28 16:44:14 +0300

    test: new

--- simple_test.go
+++ simple_test.go
@@ -8,14 +8,35 @@ import (
 
 func TestAdd_Zero(t *testing.T) {
 	expected := 0
-
 	actual := Add(0, 0)
+
 	assert.Equal(t, expected, actual)
+}
+
+func TestAdd_One(t *testing.T) {
+	expected := 1
+	actual := Add(1, 0)
+
+	assert.Equalf(t, expected, actual, "Not equal to 1: %d != %d", expected, actual)
 }
 
 func TestAdd_Svn(t *testing.T) {
 	expected := 7
-
 	actual := Add(3, 4)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestAdd_MinusToZero(t *testing.T) {
+	expected := 0
+	actual := Add(-4, 4)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestAdd_ToMinus(t *testing.T) {
+	expected := -10
+	actual := Add(-10, 0)
+
 	assert.Equal(t, expected, actual)
 }
